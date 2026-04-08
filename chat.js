0ca50f5b9bb1b9c2215e86b023d895779860fcad
robertmoa <smartrob666@gmail.com>
2026-04-09 01:05:39 +0800

    some functionality and intro messages to chat mainly

--- chat.js
+++ chat.js
@@ -8,21 +8,36 @@ const chatInput = document.getElementById('input_send');
 const chatBox = document.getElementById('chat_box')
 //this checks when you click send, and displays your message, then resets the chat input, i duplicated it to test for the enter key
 chatButton.addEventListener('click', function() {
-    displayMessage(mycat.username,chatInput.value)
+    if(chatInput.value == "/h")
+    {
+        displayMessageStyled("CatQuest","Use /w to whisper messages, and the W/A/S/D Keys to move around, check out the shop for the latest items!","text-info fst-italic")
+    }
+    else if (chatInput.value.slice(0,3) == "/w ")
+    {
+        displayMessageStyled(mycat.username,chatInput.value.slice(3),"text-secondary fst-italic")
+
+    }
+    else
+    {
+        displayMessage(mycat.username,chatInput.value)
+    }
     chatInput.value = ''
-})
+    }
+)
 
 
 function displayMessage(userid,txt)
 {
     if (txt == '') return "no text!"
-    if (txt.slice(0,3) == "/w ")
-    {
-        chatBox.innerHTML+= `<span class="text-secondary fst-italic">(${userid}): ${txt.slice(3)}</span><br>`;
-    }
-    else
-    {
-        chatBox.innerHTML+= `(${userid}): ${txt}<br>`;
-    }
+    chatBox.innerHTML+= `(${userid}): ${txt}<br>`;
     chatBox.scrollTop = chatBox.scrollHeight;
 }
+function displayMessageStyled(userid,txt,bsstyle)
+{
+    if (txt == '') return "no text!"
+    chatBox.innerHTML+= `<span class="${bsstyle}">(${userid}): ${txt}</span><br>`;
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+
+displayMessageStyled("CatQuest","Welcome to CatQuest!, use /h for help","text-info fst-italic")
