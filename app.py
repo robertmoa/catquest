be1147d450ea15b027a7cb67d2eb4ff712f1471c
VazLM <lachlan.vaz@icloud.com>
2026-04-26 15:04:42 +0800

    added a very basic login page that accepts any username and password for now, plan to make it better later

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
+def root():
+    return render_template("login.html")
+
+@app.route("/main")
 def home():
     return render_template("main.html")
 
@@ -13,6 +17,14 @@ def dungeon():
 @app.route("/shop")
 def shop():
     return render_template("shop.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return redirect(url_for("home"))
+
+    return render_template("login.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
