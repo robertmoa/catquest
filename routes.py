4be5ddea49198b7372c24e8b787703efbfc9d787
VazLM <lachlan.vaz@icloud.com>
2026-04-30 11:51:05 +0800

    updated routes.py to load the login page first, then main after logging in - the logic for that doesnt work yet

--- routes.py
+++ routes.py
@@ -1,12 +1,25 @@
-from flask import render_template,Blueprint
-main = Blueprint("main",__name__)
-@main.route("/")
+from flask import render_template, Blueprint, redirect, request, url_for
+
+main = Blueprint("main", __name__)
+
+
+@main.route("/", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        return redirect(url_for("main.home"))
+
+    return render_template("login.html")
+
+
+@main.route("/home")
 def home():
     return render_template("main.html")
+
 
 @main.route("/dungeon")
 def dungeon():
     return render_template("dungeon.html")
+
 
 @main.route("/shop")
 def shop():
